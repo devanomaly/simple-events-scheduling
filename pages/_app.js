5de9aa0dac21bfea5113cc3e40d96d149f168a7c
devanomaly <rafaelfpcosta@gmail.com>
2021-10-01 11:31:31 -0300

    layout wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import Layout from '../components/layout/layout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // wrapping a global layout! to ALL PAGES!
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
