3457ad0e1a8658f92e138e2e22b4469d811f06c9
devanomaly <rafaelfpcosta@gmail.com>
2021-10-01 10:49:46 -0300

    minor fixes

--- components/events/EventList.jsx
+++ components/events/EventList.jsx
@@ -5,15 +5,16 @@ export default function EventList(props) {
   const { items } = props
   return (
     <ul className={styles.list}>
-      {items.map(event => <li>
-        <EventItem
-          key={event.id}
-          id={event.id}
-          title={event.title}
-          location={event.location}
-          image={event.image}
-          date={event.date}
-        /></li>)}
+      {items.map(event => (
+        <li key={event.id}>
+          <EventItem
+            id={event.id}
+            title={event.title}
+            location={event.location}
+            image={event.image}
+            date={event.date}
+          /></li>)
+      )}
     </ul>
   )
 }
