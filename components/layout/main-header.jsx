c8a1e32c856ec44c239e82ee3e5f0a5fca40ed2a
devanomaly <rafaelfpcosta@gmail.com>
2021-10-01 11:31:47 -0300

    forgot about these

---
+++ components/layout/main-header.jsx
@@ -0,0 +1,22 @@
+// import  from '';
+import Link from "next/dist/client/link"
+import classes from "./main-header.module.css"
+
+export default function MainHeader () {
+
+return (
+  <header className={classes.header}>
+    <div className={classes.logo}>
+    <Link href="/">NextEvents</Link>
+    </div>
+    <nav className={classes.navigation}>
+      <ul>
+        <li>
+          <Link href="/events">Browse All Events</Link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+)
+
+}
